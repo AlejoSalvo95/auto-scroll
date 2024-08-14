42cb0249a1ea4f0cebb0dec1abf72bbf366c5d64
Ale <alejo-salvo@hotmail.com>
2024-08-14 16:48:29 +0200

    Simplify code

--- src/pomodoro_window.py
+++ src/pomodoro_window.py
@@ -9,60 +9,20 @@ class PomodoroWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
-        self.countdownTimer = QTimer(self)
-        self.countdownTimer.timeout.connect(self.update_countdown)
-        self.remainingTime = 0
-        self.fast = False
 
     def initUI(self):
-        self.setWindowTitle('Pomodoro Timer')
+        self.setWindowTitle('Scroll Window')
         self.resize(300, 100)
         self.move(50, get_window_height() - 350)
 
         self.notifButton = QPushButton('Slow', self)
-        self.notifButton.clicked.connect(lambda: self.scroll_slow())
+        self.notifButton.clicked.connect(lambda: start_scrolling(-1, 1))
 
-        self.notifButton2 = QPushButton('SFast', self)
-        self.notifButton2.clicked.connect(lambda: self.scroll_fast())
+        self.notifButton2 = QPushButton('Fast', self)
+        self.notifButton2.clicked.connect(lambda: start_scrolling(-1, 0.5))
 
         layout = QVBoxLayout()
         layout.addWidget(self.notifButton)
         layout.addWidget(self.notifButton2)
 
         self.setLayout(layout)
-
-    def increase_timer(self):
-        self.remainingTime += 60
-
-    def decrease_timer(self):
-        if self.remainingTime > 60:
-            self.remainingTime -= 60
-
-    def scroll_slow(self):
-        self.scroll()
-
-    def scroll_fast(self):
-        self.fast = True
-        self.scroll()
-
-    def scroll(self):
-        duration = self.set_main_window()
-        self.countdownTimer.start(1000)
-        start_scrolling()
-
-    def set_main_window(self):
-        if self.fast == True:
-            duration = 900000
-        else:
-            duration = 5000
-        self.remainingTime = duration // 1000
-        return duration
-
-    def set_notification_background(self, window, color):
-        palette = window.palette()
-        palette.setColor(QPalette.Window, color)
-        window.setPalette(palette)
-
-    def update_countdown(self):
-        self.remainingTime -= 1
-
--- src/scroll.py
+++ src/scroll.py
@@ -1,7 +1,7 @@
 import pyautogui
 import time
 
-def start_scrolling(scroll_amount=-2, scroll_times=200, delay_between_scrolls=1):
-    for _ in range(scroll_times):
+def start_scrolling(scroll_amount=-2, delay_between_scrolls=1):
+    for _ in range(500):
         pyautogui.scroll(scroll_amount)
         time.sleep(delay_between_scrolls)
