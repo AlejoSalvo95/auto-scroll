8d7bc6192a148bd5138bc0323e3bc17b80daa2ae
System Administrator <alejo-salvo@hotmail.com>
2024-09-01 18:17:18 +0200

    Added buttons

--- src/pomodoro_window.py
+++ src/pomodoro_window.py
@@ -12,17 +12,29 @@ class PomodoroWindow(QWidget):
 
     def initUI(self):
         self.setWindowTitle('Scroll Window')
-        self.resize(300, 100)
+        self.resize(300, 400)
         self.move(50, get_window_height() - 350)
 
-        self.notifButton = QPushButton('Slow', self)
-        self.notifButton.clicked.connect(lambda: start_scrolling(-1, 1))
+        self.notifButton = QPushButton('Slow down', self)
+        self.notifButton.clicked.connect(lambda: start_scrolling(-2, 2))
 
-        self.notifButton2 = QPushButton('Fast', self)
-        self.notifButton2.clicked.connect(lambda: start_scrolling(-1, 0.5))
+        self.notifButton1 = QPushButton('Whatsapp stickers down', self)
+        self.notifButton1.clicked.connect(lambda: start_scrolling(-9, 1))
 
+        self.notifButton2 = QPushButton('Whatsapp stickers up', self)
+        self.notifButton2.clicked.connect(lambda: start_scrolling(9, 1))
+
+        self.notifButton3 = QPushButton('Slow up', self)
+        self.notifButton3.clicked.connect(lambda: start_scrolling(2, 2))
+
+
+        self.notifButton4 = QPushButton('Top up', self)
+        self.notifButton4.clicked.connect(lambda: start_scrolling(500, 1))
         layout = QVBoxLayout()
         layout.addWidget(self.notifButton)
+        layout.addWidget(self.notifButton1)
         layout.addWidget(self.notifButton2)
+        layout.addWidget(self.notifButton3)
+        layout.addWidget(self.notifButton4)
 
         self.setLayout(layout)
